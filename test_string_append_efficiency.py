9c4bf92bb061f6ae873f51967b46ea95c074f2bc
Litkowiak_d <DLitkowiak_d@fhlbc.com>
2022-02-22 11:59:05 -0600

    Adding speed test for string concatenation vs list join

---
+++ test_string_append_efficiency.py
@@ -0,0 +1,62 @@
+import time
+
+
+
+
+def python_append_string(size):
+    x = ''
+    for _ in range(size):
+        x += 'Hello'
+    return x
+
+def python_append_list(size):
+    x = []
+    for _ in range(size):
+        x.append('Hello')
+    return ''.join(x)
+
+
+
+if __name__ == '__main__':
+    start_size  =   1
+    # WARNING! THIS WILL TAKE A FEW MINUTES TO RUN!
+    end_size    =   1000000
+
+    # current_size == number of appends to the list/string
+    current_size = start_size
+    while (current_size := current_size * 10) <= end_size:
+        
+        append_string_begin = time.time()
+        runtimes_string = []
+        for i in range(100):
+
+            # get runtime of python_append_string
+            start = time.time()
+            python_append_string(current_size)
+            end = time.time()
+
+            runtimes_string.append(end-start)
+        append_string_end = time.time()
+
+        append_list_begin = time.time()
+        runtimes_list = []
+        for i in range(100):
+
+            # get runtime of python_append_list
+            start = time.time()
+            python_append_list(current_size)
+            end = time.time()
+
+            runtimes_list.append(end-start)
+        append_list_end = time.time()
+
+
+        print( "====================================================================")
+        print(f"Average runtime for append_string with string size {current_size}: {sum(runtimes_string) / len(runtimes_string)}")
+        print(f"Total runtime for append_string is {append_string_end - append_string_begin}")
+        print(f"Average runtime for append_list with string size {current_size}: {sum(runtimes_list) / len(runtimes_list)}")
+        print(f"Total runtime for append_list is {append_list_end - append_list_begin}")
+        print( "====================================================================")
+
+
+
